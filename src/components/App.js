5747fcfbb331fbb268c23aea56b0b3a534f427ca
Quinten Clause <quinten.clause@studioemma.eu>
2018-03-27 16:37:47 +0200

    Advance slide on body click

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,51 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 import Main from './Main'
 import Footer from './Footer'
+import SlidesAPI from '../api'
 
-const App = () => (
-    <div className="page-wrapper">
-        <Main />
-        <Footer />
-    </div>
-)
+class App extends React.Component {
 
-export default App
+    constructor() {
+        super();
+        this.bodyClick = this.bodyClick.bind(this);
+    }
+
+    nextSlide() {
+        this.slideIndex++;
+        if (this.slideIndex >= this.slides.length) {
+            this.slideIndex = -1;
+        }
+        if (this.slideIndex === -1) {
+            this.props.history.push('/');
+        } else {
+            this.props.history.push('/slides/' + this.slides[this.slideIndex].url);
+        }
+    }
+
+    componentDidMount() {
+        this.slides = SlidesAPI.all();
+        this.slideIndex = -1;
+        document.body.addEventListener("click", this.bodyClick);
+    }
+
+    componentWillUnmount () {
+        document.body.removeEventListener('click', this.bodyClick);
+    }
+
+    bodyClick() {
+        console.log('body click');
+        this.nextSlide();
+    }
+
+    render() {
+        return (
+            <div className="page-wrapper">
+                <Main />
+                <Footer />
+            </div>
+        )
+    }
+}
+
+export default withRouter(App)
